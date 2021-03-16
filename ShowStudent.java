93caf42489651b38403746675444e3a3fe83c676
Amiel <s1920907@usls.edu.ph>
2021-03-16 15:46:05 +0800

    Second

---
+++ ShowStudent.java
@@ -0,0 +1,16 @@
+public class ShowStudent {
+    
+    
+    public static void main(String[] args) {
+        
+        Student student = new Student();
+        student.setStudentID(1);
+        student.setCreditHours(12);
+        student.setPoints();
+        student.setGradePointAv();
+        student.studentInput();
+    }
+    
+   
+    
+}
